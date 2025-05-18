d19a196524af65524d32a755f71f36d1bddd408c
Sihouette-Of-Past <23751@burnside.school.nz>
2025-05-18 19:48:29 +1200

    can convert units to SI units

--- units_converter.py
+++ units_converter.py
@@ -1,19 +1,27 @@
 """Unit Converter application with Database by Amy Lian 15/5/2025"""
 
+#imports sqlite
 import sqlite3
 
-db = sqlite3.connect('units_converter.db')
+def convert(num, unit1):
+    #connecting database
+    db = sqlite3.connect('units_converter.db')
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-#sql commands
-sql = "SELECT * FROM Units"
+    #sql commands
+    sql = "SELECT ?*to_si_unit FROM Units WHERE unit_name = ?;"
 
-cursor.execute(sql)
+    cursor.execute(sql, (num,unit1))
 
-info = cursor.fetchall()
+    info = cursor.fetchall()
 
-print(info)
+    print(info)
 
-db.close()
+    db.close()
 
+if __name__ == "__main__":
+    print("Hi")
+    number = input()
+    start_unit = input()
+    convert(number, start_unit)
